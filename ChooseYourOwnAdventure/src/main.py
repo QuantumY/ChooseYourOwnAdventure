2c6e28321835c04fb429d7975f1dd0e264c30335
Jeff Doss <jdosstech@gmail.com>
2018-11-24 11:09:38 -0500

    Starting parser

--- ChooseYourOwnAdventure/src/main.py
+++ ChooseYourOwnAdventure/src/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import story_parser
+
 def main():
     """ Main program """
     # Code goes over here.
---
+++ ChooseYourOwnAdventure/src/story_parser.py
@@ -0,0 +1,10 @@
+import re
+import choice
+
+def story_parser(fileName):
+    # Open file
+
+
+
+    return choice;
+
