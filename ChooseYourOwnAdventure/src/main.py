7b7d37c1f0ff730da36c3006f44c409b381b18d2
Jeff Doss <jdosstech@gmail.com>
2018-11-24 10:57:51 -0500

    Adding main file

--- ChooseYourOwnAdventure/src/main.py
+++ ChooseYourOwnAdventure/src/main.py
@@ -1,1 +1,11 @@
-main.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    """ Main program """
+    # Code goes over here.
+    print "What's Up"
+    return 0
+
+if __name__ == "__main__":
+    main()
