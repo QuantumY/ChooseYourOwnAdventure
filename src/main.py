bc2bdeb22c361d29ee087485c9579e31d4814ac8
Jeff Doss <jdosstech@gmail.com>
2018-12-15 18:49:08 -0500

    Cleanup

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
-from choice import choice
+from scenario import scenario
 from story_parser import story_parser
 
 storyParser = story_parser()
 
 theStory = storyParser.parseStoryFile("../Stories/LairOfTheGoblinHoard.md")
-
-# Test the parser
-theStory.echo()
 
 theStory.prompt()
